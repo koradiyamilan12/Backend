784d943a4dd55709a41dc189f387e00de8d7bbf3
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-04-17 12:07:19 +0530

    node package manager

---
+++ part-1/3.node-package-manager/index.js
@@ -0,0 +1,6 @@
+const lodash = require("lodash");
+
+const names = ["milan", "bhavin", "ankit", "meet", "darshil"];
+const capitalize = lodash.map(names, lodash.capitalize);
+
+console.log(capitalize);
