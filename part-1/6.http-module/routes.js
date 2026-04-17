46f2ff30c753339928acfd98329a3bb492788f9e
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-04-17 13:08:11 +0530

    HTTP module

---
+++ part-1/6.http-module/routes.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Home page");
+  } else if (url === "/projects") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("project page");
+  } else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("this page can not found!");
+  }
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
